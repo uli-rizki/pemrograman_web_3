a48afa0b6687c84eec884ac96b1ef971c859dd72
uli <ulirizki@gmail.com>
2025-03-04 09:49:09 +0700

    update mahasiswa

--- kasus_1/Mahasiswa.php
+++ kasus_1/Mahasiswa.php
@@ -1,17 +1,22 @@
 <?php
 
-require('koneksi.php');
-
 class Mahasiswa {
     public $nim;
     public $nama_lengkap;
     public $jenis_kelamin;
+    public $db;
+
+    public function __construct()
+    {
+        include 'koneksi.php';
+        $this->db = $koneksi;
+    }
 
     public function tampil()
     {
         $query = "SELECT * FROM mahasiswa";
-        $hasil = $koneksi->query($query);
-        $koneksi->close();
+        $hasil = $db->query($query);
+        $db->close();
 
         return $hasil;
     }
@@ -19,14 +24,14 @@ class Mahasiswa {
     public function simpan($data)
     {
         $this->nim = isset($_POST['nim']) ? $_POST['nim'] : "";
-        $this->nama_lengkap = isset($_POST['nama_lengkap']) ? $_POST['nam_lengkap'] : "";
+        $this->nama_lengkap = isset($_POST['nama_lengkap']) ? $_POST['nama_lengkap'] : "";
         $this->jenis_kelamin = isset($_POST['jenis_kelamin']) ? $_POST['jenis_kelamin'] : "";
 
         $query = "INSERT INTO mahasiswa (nim, nama_lengkap, jenis_kelamin)
                 VALUES ('".$this->nim."', '".$this->nama_lengkap."', '".$this->jenis_kelamin."')";
 
-        $hasil = $koneksi->query($query);
-        $koneksi->close();
+        $hasil = $this->db->query($query);
+        $this->db->close();
 
         return $hasil;
     }
--- kasus_1/mahasiswa_tambah.php
+++ kasus_1/mahasiswa_tambah.php
@@ -1,4 +1,7 @@
 <?php 
+ini_set('display_errors', 1);
+ini_set('display_startup_error', 1);
+error_reporting(E_ALL);
 require('Mahasiswa.php');
 
 if($_POST) {
